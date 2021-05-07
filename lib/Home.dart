20feeee072111ad6f181a7818f4ca6f0c3426796
Lucas Santos <lucasantos603@gmail.com>
2021-05-07 07:56:49 -0300

    Create Appbar

---
+++ lib/Home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey, opacity: 1),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "images/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        // Text(
+        //   "YouTube",
+        //   style: TextStyle(color: Colors.black),
+        // ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.videocam),
+              onPressed: () => {print("acao: videocam")}),
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => {print("acao: pesquisa")}),
+          IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () => {print("acao: conta")})
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
