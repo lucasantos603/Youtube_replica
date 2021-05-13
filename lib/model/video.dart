6c8202829cd84fd714e5f7f3cbfd9c0a1e654f77
Lucas Santos <lucasantos603@gmail.com>
2021-05-13 06:47:08 -0300

    Created Api and model

---
+++ lib/model/video.dart
@@ -0,0 +1,15 @@
+class video {
+  String id;
+  String titulo;
+  String descricao;
+  String imagem;
+  String canal;
+
+  video({
+    this.id,
+    this.titulo,
+    this.descricao,
+    this.imagem,
+    this.canal,
+  });
+}
